d5a15fef8c826752c1d97f9d102f723a937e8e30
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-01-02 09:12:17 -0700

    added loads of stuff to pokemonpanel.

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,52 @@
 package pokemon.view;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import pokemon.controller.PokemonController;
+
 public class PokemonPanel
 {
-
+	private PokemonController appController;
+	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextField discriptionArea;
+	private JTextField typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokemonDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
+	
+	private void updatePokemonInfo(int index)
+	{
+		nameField.setText(appController.getPokedex().get(index).getName());;
+		evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
+		numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+		attackField.setText(appController.getPokedex().get(index).getAttackPoints()+ "");
+		healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+		modifierField.setText(appController.getPokedex().get(index).getEhancementModifier() + "");				
+	}
 }
