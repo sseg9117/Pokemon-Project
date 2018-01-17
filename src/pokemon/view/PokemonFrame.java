4d50d8f0a74c05f943aee06d8c1f28d34eb6dd32
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-01-17 09:36:07 -0700

    added frame.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,5 +1,7 @@
 package pokemon.view;
 
+import java.awt.Panel;
+
 import pokemon.controller.PokemonController;
 
 public class PokemonFrame
@@ -7,7 +9,37 @@ public class PokemonFrame
 	public PokemonController appController;
 	public PokemonFrame(PokemonController appController)
 	{
+		super();
 		this.appController = appController;
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+		this.setSize(500,500);
+		this.setTitle("Pokedex");
+		this.setContentPane();
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	private void setContentPane()
+	{
+
+	}
+	private void setVisible(boolean b)
+	{
+		
+	}
+	private void setResizable(boolean b)
+	{
+		
+	}
+	private void setTitle(String string)
+	{
+		
+	}
+	private void setSize(int i, int j)
+	{
+		
 	}
 	
 }
